33dc6f93258833aebe4ef823565b7b720cf75df3
laurielim <laurie.limsam@gmail.com>
2021-04-13 10:35:44 +0300

    fetch formatted data w javascript

---
+++ day-05-pokemon-list-fullstack-app/script.js
@@ -0,0 +1,30 @@
+// Wrap script inside self calling function
+(function () {
+  document.addEventListener("DOMContentLoaded", executeScript);
+
+  function executeScript() {
+    // Make API call
+    fetch("formatted_pokemon.php")
+      // Parse JSON
+      .then((response) => response.json())
+      .then((data) => {
+        // Assign array of pokemonts to a variable
+        let pokemons = data;
+        // Iterate through each Pokemon to display them
+        pokemons.forEach(addPokemonName);
+      });
+
+    /**
+     * Given a name, it will add it to poke-list as an anchor tag.
+     * @param {String} pokemon
+     */
+    const addPokemonName = (pokemon, index) => {
+      // Create new div for pokemon
+      let listedPokemon = document.createElement("div");
+      // Add an anchor tag inside div with pokemon name
+      listedPokemon.innerHTML = `<p>${index}. ${pokemon.name}, url: <a href = "${pokemon.url}">${pokemon.url}</a></p>`;
+      // Append anchor to poke-list section
+      document.getElementById("poke-list").appendChild(listedPokemon);
+    };
+  }
+})();
