ee4d0124932114dd4dcbde7d8e8b13d960781868
laurielim <laurie.limsam@gmail.com>
2021-04-16 10:30:01 +0300

    include form validation to example

--- day-04-contact-form/class-example/contact.php
+++ day-04-contact-form/class-example/contact.php
@@ -4,11 +4,16 @@
     print_r($_POST);
     echo '</pre>';
 
+    if (isset($_POST['name']) && isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+    $to = 's2100139@edu.bc.fi';
+    $subject = htmlspecialchars($_POST['username']) . 'with email' . htmlspecialchars($_POST['email']) . ' has sent an email';
     $message = 'Message from ' . $_POST['username'] . ': ' . $_POST['message']; 
     
-    $sent_status = mail('s2100139@edu.bc.fi', 'Sent from test PHP app', $message);
+    $sent_status = mail($to, $subject, $message);
 
     if ($sent_status): echo 'Your message has been sent';
     else: echo 'Oops! Something went wrong. Your message was not sent';
     endif;
+}
 ?>
