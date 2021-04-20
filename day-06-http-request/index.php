c2b9866084b3de59bc4358463c5feacaa9e1a43f
laurielim <laurie.limsam@gmail.com>
2021-04-20 19:57:25 +0300

    add class example

---
+++ day-06-http-request/index.php
@@ -0,0 +1,65 @@
+<?php
+    /**
+     * GET /recipes
+     * return all recipes
+     * 
+     * GET /recipes/{id}
+     * return recipes with the specific id if found
+     * 
+     * POST /recipes
+     * parameters must exist
+     * if we have id, name, ingredients, difficulty
+     * add a new recipe to our recipes array and save the json
+     * 
+     * PUT /recipes
+     * parameters must exist
+     * if we have id, name, ingredients, difficulty
+     * find if there is a recipe with such id, if found, update
+     * 
+     * DELETE /recipes/{id}
+     * delete recipe with the matching id if found
+     * 
+     */
+
+    $json_data = file_get_contents('data.json');
+
+    // Assign request method to a variable
+    $request_method = $_SERVER['REQUEST_METHOD'];
+    // Assign request uri to a variable
+    $uri = $_SERVER['REQUEST_URI'];
+    // Breaks down the uri into the path and query
+    $parts = parse_url($uri);
+    // Breaks down the query into an array of parameters
+    parse_str($parts['query'], $params);
+    // Breaks down the path into an array
+    $exploded_parts = explode('/', $parts['path']);
+
+   switch($request_method) {
+        case 'GET':
+            get_recipes();
+            break;
+        case 'POST':
+            // add_new_recipe();
+            break;
+        case 'PUT':
+            // update_recipe();
+            break;
+        case 'DELETE':
+            // remove_recipe();
+            break;
+        default: 
+            echo json_encode(array('message' => 'An error has occured'));
+            break;
+    }
+
+    function get_recipes(){
+        if (!isset($GLOBALS['exploded_parts'][2])) {
+            echo $GLOBALS['data'];
+        } else {
+            echo json_encode(array('message' => 'Your recipe'));
+        }
+    }
+    // echo '<pre>';
+    // print_r($_SERVER);
+    // echo '</pre>';
+?>
